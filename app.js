6616fecb5c456893f0ab459d79e1147725e1bce1
AmirHosein Ravaghi <amirhravaghi@gmail.com>
2023-08-16 12:40:30 +0330

    Debug 2

--- app.js
+++ app.js
@@ -25,9 +25,8 @@ bot.on('message', (ctx) => {
     
     // let debugChatIds = [98484342,727539725,894815485,77363322,885548849,1771771581];
     // if(!debugChatIds.includes(ctx.chat.id)) return 0;
-    
-    ctx.telegram.getChatMember(config.get("sponsor_channel"),ctx.chat.id).then(() => {
-
+    ctx.telegram.getChatMember(config.get("sponsor_channel"),ctx.chat.id).then((value) => {
+        console.log(value);
         // DB Connection
         mongoose.set('strictQuery', true);
         let connectionString = `mongodb://${config.get('db.user')}:${config.get('db.pass')}@${config.get('db.host')}/${config.get('db.db_name')}?authSource=admin`;
